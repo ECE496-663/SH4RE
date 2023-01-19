95d46ffa51997a8846bcbed0580d898ac8e9420f
Wasif Butt <wasifahmadbutt@gmail.com>
2023-01-18 19:27:01 -0500

    reverted changes to Account.swift

--- SH4RE/SH4RE/Account/Account.swift
+++ SH4RE/SH4RE/Account/Account.swift
@@ -4,21 +4,14 @@
 //
 //  Created by Hannah Brooks on 2022-11-24.
 //
-
 import SwiftUI
 import Firebase
 
 class Account: ObservableObject {
-    @AppStorage("UID") var username: String = (UserDefaults.standard.string(forKey: "UID") ?? "")
-    
+
     var body: some Scene {
         WindowGroup {
-            if (username.isEmpty) {
-                GuestView()
-            }
-            else {
-                AccountView()
-            }
+            AccountView()
         }
     }
 }
