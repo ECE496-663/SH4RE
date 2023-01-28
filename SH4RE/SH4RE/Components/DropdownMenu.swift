377fc71dbcddf7909f158e8030803435b798f2eb
Americo Barros <americobarros8@gmail.com>
2023-01-28 17:26:30 -0500

    Complete drop down styling

--- SH4RE/SH4RE/Components/DropdownMenu.swift
+++ SH4RE/SH4RE/Components/DropdownMenu.swift
@@ -7,27 +7,59 @@
 
 import SwiftUI
 
-struct CustomDisclosureGroupStyle: DisclosureGroupStyle {
+
+struct DropdownStyle: DisclosureGroupStyle {
     let button: some View = Image(systemName: "chevron.up")
         .fontWeight(.medium)
         .foregroundColor(.black)
+    var border: some View {
+      RoundedRectangle(cornerRadius: 8)
+        .strokeBorder(
+            .gray,
+          lineWidth: 1
+        )
+    }
     func makeBody(configuration: Configuration) -> some View {
-        HStack {
-            configuration.label
-            Spacer()
-            button
-                .rotationEffect(.degrees(configuration.isExpanded ? 180 : 0))
-        }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            withAnimation {
-                configuration.isExpanded.toggle()
-            }
-        }
-        if configuration.isExpanded {
-            configuration.content
-            //                .padding()//.leading, 30)
+        ZStack (alignment: .topLeading){
+            if configuration.isExpanded {
+                VStack {
+                    //Spacer makes the drop down content visible from behind main label
+                    Spacer().frame(height: 52)
+                    configuration.content
+                }
+                
                 .disclosureGroupStyle(self)
+                .background(border)
+                .background(.white)
+                .cornerRadius(8)
+            }
+            HStack {
+                configuration.label
+                Spacer()
+                button
+                    .rotationEffect(.degrees(configuration.isExpanded ? 180 : 0))
+            }
+            // Text alignment.
+            .multilineTextAlignment(.leading)
+            // Cursor color.
+            .accentColor(.primaryDark)
+            // Text color.
+            .foregroundColor(.black)
+            // TextField spacing.
+            .padding(.vertical, 16)
+            .padding(.horizontal, 20)
+            // TextField border
+            .background(border)
+            .background(.white)
+            .cornerRadius(8)
+            
+            
+            .contentShape(Rectangle())
+            .onTapGesture {
+                withAnimation {
+                    configuration.isExpanded.toggle()
+                }
+            }
         }
     }
 }
@@ -38,39 +70,45 @@ struct DropdownMenu: View {
     @Binding var selection: String
     var useClear: Bool = false
     var clearValue: String = ""
-    @State var expanded: Bool = false
+    @State var expanded: Bool = true
     var body: some View {
-        GroupBox{
-            DisclosureGroup(selection == clearValue ? label : selection, isExpanded: $expanded) {
-                ScrollView{
-                    if (useClear){
-                        Divider()
-                        HStack{
-                            Text("Clear")
-                        }
-                        .onTapGesture {
-                            selection = clearValue
-                            withAnimation {
-                                expanded.toggle()
-                            }
+        DisclosureGroup(selection == clearValue ? label : selection, isExpanded: $expanded) {
+            ScrollView{
+                if (useClear){
+                    Divider().opacity(0)
+                    HStack{
+                        Text("Clear")
+                    }
+                    //The frame is to make the whole width of the button clickable
+                    .frame(maxWidth: screenSize.width)
+                    //The frame alone is not enough, to make the frame clickable, a background must be added
+                    .background(Color.white.opacity(0.001))
+                    .onTapGesture {
+                        selection = clearValue
+                        withAnimation {
+                            expanded.toggle()
                         }
                     }
-                    ForEach(options, id: \.self){ category in
-                        Divider()
-                        HStack{
-                            Text(category)
-                        }
-                        .onTapGesture {
-                            selection = category
-                            withAnimation {
-                                expanded.toggle()
-                            }
-                        }
-                    }
+                    Divider()
                 }
-                .frame(maxHeight: 140)
-            }.disclosureGroupStyle(CustomDisclosureGroupStyle())
-        }
+                ForEach(options, id: \.self){ category in
+                    HStack{
+                        Text(category)
+                            .scaledToFill()
+                    }
+                    .frame(maxWidth: screenSize.width)
+                    .background(Color.white.opacity(0.001))
+                    .onTapGesture {
+                        selection = category
+                        withAnimation {
+                            expanded.toggle()
+                        }
+                    }
+                    Divider()
+                }
+            }
+            .frame(maxHeight: 140)
+        }.disclosureGroupStyle(DropdownStyle())
     }
 }
 
